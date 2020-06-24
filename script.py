557a2143dc115f8712f7c9ae631c1da5ac096178
Kevin Plazarte <kplazarte@est.ups.edu.ec>
2020-06-24 08:17:41 -0500

    Agregada archivo texto

--- script.py
+++ script.py
@@ -2,7 +2,8 @@
 #Autor: Kevin Plazarte
 # Fecha: 24-06-2020
 def main():
-    print("Hola Mundo")
+    nombre= input("Ingresa tu nombre: ")
+    print("Hola ", nombre, "Bienvenido a Git")
     
 if __name__== "__main__":
     main()
