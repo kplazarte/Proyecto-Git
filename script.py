eaf9c3cc9b688c667bfaf89933ba658507f0a9b7
Daniela Campoverde <dcampoverdev2@est.ups.edu.ec>
2020-06-27 22:00:01 -0500

    Agregado cambios al proyecto

--- script.py
+++ script.py
@@ -4,6 +4,32 @@
 def main():
     nombre= input("Ingresa tu nombre: ")
     print("Hola ", nombre, "Bienvenido a Git")
-    
+    n1=float(input("Ingrese un numero: "))
+n2=float(input("Ingrese un numero: "))
+op=input("Ingrese una operacion: ")
+
+def suma(n1,n2):
+    return n1+n2
+
+def resta(n1,n2):
+    return n1-n2
+
+def multiplicacion(n1,n2):
+    return n1*n2
+
+def division(n1,n2):
+    return n1/n2
+
+if(op == '+'):
+    print("El valor al Sumar es:", suma(n1,n2))
+
+if (op == '-'):
+    print( "El valor al Restar es:", resta( n1, n2 ) )
+
+if (op == '*'):
+    print( "El valor al Multiplicar es:", multiplicacion( n1, n2 ) )
+
+if (op == '/'):
+    print( "El valor al Dividir es:", division( n1, n2 ) )
 if __name__== "__main__":
     main()
